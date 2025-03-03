e38b3bb53ddaa5e8e4232b9311afd53e263f1936
hilleremilia <emilia.hiller@equiqo.com>
2025-03-03 14:02:54 +0100

    remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { View } from 'react-native';
 
 function App(): React.JSX.Element {
   return (
     <View/>
   );
 }
-
-const styles = StyleSheet.create({
-});
 
 export default App;
